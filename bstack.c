bcaebd71c2d7ea535998ca1d8a00e0eceb5cfa44
Devin J. Pohly <djpohly@gmail.com>
2016-01-30 03:44:33 -0500

    rewrite bstack to match tile

--- bstack.c
+++ bstack.c
@@ -1,29 +1,24 @@
 static void
 bstack(Monitor *m) {
-	int x, y, h, w, mh;
-	unsigned int i, n;
+	unsigned int i, n, w, mh, mx, tx;
 	Client *c;
 
-	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if(n == 0)
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	if (n == 0)
 		return;
-	/* master */
-	c = nexttiled(m->clients);
-	mh = m->mfact * m->wh;
-	resize(c, m->wx, m->wy, m->ww - 2 * c->bw, (n == 1 ? m->wh : mh) - 2 * c->bw, 0);
-	if(--n == 0)
-		return;
-	/* tile stack */
-	x = m->wx;
-	y = (m->wy + mh > c->y + c->h) ? c->y + c->h + 2 * c->bw : m->wy + mh;
-	w = m->ww / n;
-	h = (m->wy + mh > c->y + c->h) ? m->wy + m->wh - y : m->wh - mh;
-	if(w < topgap)
-		w = m->ww;
-	for(i = 0, c = nexttiled(c->next); c; c = nexttiled(c->next), i++) {
-		resize(c, x, y, /* remainder */ ((i + 1 == n)
-		       ? m->wx + m->ww - x - 2 * c->bw : w - 2 * c->bw), h - 2 * c->bw, 0);
-		if(w != m->ww)
-			x = c->x + WIDTH(c);
-	}
+
+	if (n > m->nmaster)
+		mh = m->nmaster ? m->wh * m->mfact : 0;
+	else
+		mh = m->wh;
+	for (i = mx = tx = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		if (i < m->nmaster) {
+			w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
+			resize(c, m->wx + mx, m->wy, w - (2*c->bw), mh - (2*c->bw), 0);
+			mx += WIDTH(c);
+		} else {
+			w = (m->ww - tx) / (n - i);
+			resize(c, m->wx + tx, m->wy + mh, w - (2*c->bw), m->wh - mh - (2*c->bw), 0);
+			tx += WIDTH(c);
+		}
 }
