bde38fa40ca96198dbcbdbf24b4544300f4eaab1
Devin J. Pohly <djpohly@gmail.com>
2016-02-03 08:56:34 -0500

    Revert held-window/tiling changes
    This was causing in attachabove where c->mon->sel was not found in
    c->mon->clients, and I don't have time to fix it right now.
    
    This reverts the following commits:
    - 4cc1916 do math and remove extra parentheses
    - 2c7d08d update bstack to match tile
    - 3283d8b keep pointer in window when held
    - c2af1b4 resize held window to fit its opening
    - 9b660d0 match bstack to tile again
    - b8eee32 don't assume last *tiled* window is last window
    - 2311495 fix snap logic which prevented picking up windows
    - 074280a fix dragging window to last tiled position
    - e4157c9 use getrootptr
    - cacd0ab tighten up reordering logic
    - d333eed allow held window to be moved through tiles

--- bstack.c
+++ bstack.c
@@ -1,16 +1,9 @@
 static void
-bstack(Monitor *m)
-{
+bstack(Monitor *m) {
 	unsigned int i, n, w, mh, mx, tx;
-	int x, y;
-	Client *c, *held = NULL, **prevp;
+	Client *c;
 
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++)
-		if (c->isheld && getrootptr(&x, &y)) {
-			// Detach to reattach later
-			held = c;
-			detach(held);
-		}
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
 	if (n == 0)
 		return;
 
@@ -18,41 +11,14 @@ bstack(Monitor *m)
 		mh = m->nmaster ? m->wh * m->mfact : 0;
 	else
 		mh = m->wh;
-	prevp = &m->clients;
-	for (i = mx = tx = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+	for (i = mx = tx = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
 			w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
-			if (held && m->wx + mx <= x && m->wy <= y && m->wx + mx + w > x && m->wy + mh > y) {
-				resize(held, held->x, held->y, w - (2*held->bw), mh - (2*held->bw), 0);
-				held->next = c;
-				c = *prevp = held;
-				held = NULL;
-			} else
-				resize(c, m->wx + mx, m->wy, w - (2*c->bw), mh - (2*c->bw), 0);
-			mx += w;
+			resize(c, m->wx + mx, m->wy, w - (2*c->bw), mh - (2*c->bw), 0);
+			mx += WIDTH(c);
 		} else {
 			w = (m->ww - tx) / (n - i);
-			if (held && m->wx + tx <= x && m->wy + mh <= y && m->wx + tx + w > x && m->wy + m->wh > y) {
-				resize(held, held->x, held->y, w - (2*held->bw), m->wh - mh - (2*held->bw), 0);
-				held->next = c;
-				c = *prevp = held;
-				held = NULL;
-			} else
-				resize(c, m->wx + tx, m->wy + mh, w - (2*c->bw), m->wh - mh - (2*c->bw), 0);
-			tx += w;
+			resize(c, m->wx + tx, m->wy + mh, w - (2*c->bw), m->wh - mh - (2*c->bw), 0);
+			tx += WIDTH(c);
 		}
-		prevp = &c->next;
-	}
-	// If held window didn't get reattached anywhere, attach after last tiled
-	if (held) {
-		if (n <= m->nmaster) {
-			w = (m->ww - mx) / MIN(1, m->nmaster - n + 1);
-			resize(held, held->x, held->y, w - (2*held->bw), mh - (2*held->bw), 0);
-		} else {
-			w = m->ww - tx;
-			resize(held, held->x, held->y, w - (2*held->bw), m->wh - mh - (2*held->bw), 0);
-		}
-		held->next = *prevp;
-		*prevp = held;
-	}
 }
