dad9a36f2dfb1d3826c275720afdc57de08e32a0
Devin J. Pohly <djpohly@gmail.com>
2019-12-22 14:44:08 -0600

    Revert "map/unmap windows when changing tags"
    This reverts commit 41f439abb0aadf1796321cbcca7685e728023f3a.
    
    We'll give this another try.

--- dwm.c
+++ dwm.c
@@ -1699,14 +1699,14 @@ showhide(Client *c)
 		return;
 	if (ISVISIBLE(c)) {
 		/* show clients top down */
-		XMapWindow(dpy, c->win);
+		XMoveWindow(dpy, c->win, c->x, c->y);
 		if ((!c->mon->lt[c->mon->sellt]->arrange || c->isfloating) && !c->isfullscreen)
 			resize(c, c->x, c->y, c->w, c->h, 0);
 		showhide(c->snext);
 	} else {
 		/* hide clients bottom up */
 		showhide(c->snext);
-		XUnmapWindow(dpy, c->win);
+		XMoveWindow(dpy, c->win, WIDTH(c) * -2, c->y);
 	}
 }
 
@@ -1863,8 +1863,12 @@ unmapnotify(XEvent *e)
 	Client *c;
 	XUnmapEvent *ev = &e->xunmap;
 
-	if ((c = wintoclient(ev->window)) && ev->send_event)
-		setclientstate(c, WithdrawnState);
+	if ((c = wintoclient(ev->window))) {
+		if (ev->send_event)
+			setclientstate(c, WithdrawnState);
+		else
+			unmanage(c, 0);
+	}
 }
 
 void
