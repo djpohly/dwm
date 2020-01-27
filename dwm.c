e82ea6cbc71fbcea3f02458f4ca2a0802c568f1e
Devin J. Pohly <djpohly@gmail.com>
2020-01-26 19:39:10 -0600

    don't hide the Xft include

--- dwm.c
+++ dwm.c
@@ -36,6 +36,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
 #include <X11/Xutil.h>
+#include <X11/Xft/Xft.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
 #else /* XINERAMA */
@@ -44,7 +45,6 @@ typedef struct {
 	short x_org, y_org, width, height;
 } XineramaScreenInfo;
 #endif /* XINERAMA */
-#include <X11/Xft/Xft.h>
 
 #include "drw.h"
 #include "util.h"
